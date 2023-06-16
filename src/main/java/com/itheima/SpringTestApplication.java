34974a273f3a8507c849d28c9d913d1eae35dd52
copm <2071231427@qq.com>
2023-06-16 18:32:58 +0800

    111111

---
+++ src/main/java/com/itheima/SpringTestApplication.java
@@ -0,0 +1,15 @@
+package com.itheima;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+//引导类 ，SpringBoot的启动入口
+@SpringBootApplication
+public class  SpringTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringTestApplication.class, args);
+    }
+
+}
+
---
+++ src/main/java/com/itheima/controller/HelloController.java
@@ -0,0 +1,41 @@
+package com.itheima.controller;
+
+import com.itheima.bean.Person;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.env.Environment;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.awt.print.Printable;
+
+/**
+ * @Description
+ * @Author linhongqi
+ * @Data 2023/6/13 21:19
+ */
+
+@Controller
+public class HelloController {
+    @Autowired
+    private Person person;
+    @RequestMapping("/hello")
+    public String hello() {
+        return "hello SpringBoot!";
+    }
+
+
+
+    @GetMapping("/index")
+    public String index(){
+        return "index";
+    }
+
+    @GetMapping("/index2")
+    public String index2(){
+        System.out.println(person);
+        return "index2";
+    }
+}
